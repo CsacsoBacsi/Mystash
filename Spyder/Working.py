b7ebd94c3c6bc780078daac663eefcdb2c6bdd41
Csaba Riedlinger <csaba.riedlinger@gmail.com>
2026-03-04 08:04:50 +0100

    Spyder Working.py syntax fix

--- Spyder/Working.py
+++ Spyder/Working.py
@@ -93,13 +93,13 @@ MERGE INTO csaba_trg AS t USING
             current_date as start_date,
             current_date -1 as end_date 
     FROM  ua 
-    GROUP BY ua.col1, ua.col2, ua.col3, ua.val # Check all columns if they are different. Basically distinct list
-    HAVING COUNT (ua.ind1) != COUNT (ua.ind2)) AS src # Only when there is a difference. Otherwise no change, so ignore
+    GROUP BY ua.col1, ua.col2, ua.col3, ua.val -- Check all columns if they are different. Basically distinct list
+    HAVING COUNT (ua.ind1) != COUNT (ua.ind2)) AS src -- Only when there is a difference. Otherwise no change, so ignore
 ON (t.col1 || '||' || t.col2 || '||' || t.col3 || '||' || 'U' = 
-    src.col1 || '||' || src.col2 || '||' || src.col3 || '||' || src.transaction_type) # Merge key: primary key columns
-WHEN MATCHED AND t.end_date IS NULL THEN # There can be a multi-row history for a key, so leave those rows and operate on current/live row only
-    UPDATE SET end_date = src.end_date # Update only the end date (close row off). New data will be inserted.
-WHEN NOT MATCHED AND transaction_type = 'I' THEN # Row of type 'I' is only present in target, so insert
+    src.col1 || '||' || src.col2 || '||' || src.col3 || '||' || src.transaction_type) -- Merge key: primary key columns
+WHEN MATCHED AND t.end_date IS NULL THEN -- There can be a multi-row history for a key, so leave those rows and operate on current/live row only
+    UPDATE SET end_date = src.end_date -- Update only the end date (close row off). New data will be inserted.
+WHEN NOT MATCHED AND transaction_type = 'I' THEN -- Row of type 'I' is only present in target, so insert
     INSERT (col1, col2, col3, val, start_date, end_date) 
     VALUES (src.col1, src.col2, src.col3, src.val, src.start_date, NULL) 
 """
