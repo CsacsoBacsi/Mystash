78a077483a8f08675fd9e58b430b8895bf502731
Csaba Riedlinger <csaba.riedlinger@gmail.com>
2026-03-05 19:24:50 +0100

    New merge sql version

--- Spyder/Working.py
+++ Spyder/Working.py
@@ -67,45 +67,100 @@ df_lake_source= spark.read.format("delta").load("D:/Data/DeltaLake/csaba_source"
 df_lake_source.createTempView ("csaba_src")
 
 print ("Merge result:")
-spark.sql (
+
 """
-MERGE INTO csaba_trg AS t USING
-    (WITH ua AS ( 
-        SELECT col1, col2, col3, val,
-               1 AS ind1, -- Exists in source
-               NULL AS ind2
-        FROM   csaba_src 
-        WHERE  1=1  
-        UNION ALL 
-        SELECT col1, col2, col3, val, 
-               NULL AS ind1,
-               1 AS ind2 -- Exists in target
-        FROM   csaba_trg 
-        WHERE  end_date IS NULL  
-    )
-    SELECT ua.col1, ua.col2, ua.col3, ua.val, 
-           CASE WHEN COUNT (ua.ind2) = 0 AND COUNT (ua.ind1) = 1 THEN 'I' -- Row in source not in target, e.g. new row (needs inserting)
-                WHEN COUNT (ua.ind1) = 0 AND COUNT (ua.ind2) = 1 THEN 'U' -- Row in target not in source, e.g. deleted row (needs end dating)
-                ELSE                                                  'Q' -- 1 on both sides means no change, e.g. same row If more than 1, it means dupes present
-            END AS transaction_type, 
-            COUNT (ua.ind1) count_ind1, -- For recon and testing purposes only
-            COUNT (ua.ind2) count_ind2, -- For recon and testing purposes only
-            current_date as start_date,
-            current_date -1 as end_date 
-    FROM  ua 
-    GROUP BY ua.col1, ua.col2, ua.col3, ua.val -- Check all columns if they are different. Basically distinct list
-    HAVING COUNT (ua.ind1) != COUNT (ua.ind2)) AS src -- Only when there is a difference. Otherwise no change, so ignore
-ON (t.col1 || '||' || t.col2 || '||' || t.col3 || '||' || 'U' = 
-    src.col1 || '||' || src.col2 || '||' || src.col3 || '||' || src.transaction_type) -- Merge key: primary key columns
-WHEN MATCHED AND t.end_date IS NULL THEN -- There can be a multi-row history for a key, so leave those rows and operate on current/live row only
-    UPDATE SET end_date = src.end_date -- Update only the end date (close row off). New data will be inserted.
-WHEN NOT MATCHED AND transaction_type = 'I' THEN -- Row of type 'I' is only present in target, so insert
-    INSERT (col1, col2, col3, val, start_date, end_date) 
-    VALUES (src.col1, src.col2, src.col3, src.val, src.start_date, NULL) 
+insert into csaba_trg (col1, col2, col3, val, updated_at, start_date, end_date)
+values ('1','1','1',1, '2026-01-01 12:12:12', '2026-01-01 12:12:12',  '2026-01-02 22:12:11') ;
+insert into csaba_trg (col1, col2, col3, val, updated_at, start_date, end_date)
+values ('1','1','1',2, '2026-01-02 22:12:12', '2026-01-02 22:12:12', NULL) ;
+insert into csaba_trg (col1, col2, col3, val, updated_at, start_date, end_date)
+values ('2','1','1',1, '2026-01-01 12:12:12', '2026-01-01 12:12:12', NULL) ;
+insert into csaba_trg (col1, col2, col3, val, updated_at, start_date, end_date)
+values ('3','1','1',1, '2026-01-01 12:12:12', '2026-01-01 12:12:12', NULL) ;
+insert into csaba_trg (col1, col2, col3, val, updated_at, start_date, end_date)
+values ('4','1','1',1, '2026-01-01 12:12:12', '2026-01-01 12:12:12', NULL) ;
+insert into csaba_trg (col1, col2, col3, val, updated_at, start_date, end_date)
+values ('5','1','1',1, '2026-01-01 12:12:12', '2026-01-01 12:12:12', NULL) ;
+insert into csaba_trg (col1, col2, col3, val, updated_at, start_date, end_date)
+values ('8','1','1',1, '2026-01-01 12:12:12', '2026-01-01 12:12:12',  '2026-01-02 22:12:11') ;
+insert into csaba_trg (col1, col2, col3, val, updated_at, start_date, end_date)
+values ('8','1','1',2, '2026-01-02 22:12:12', '2026-01-02 22:12:12', NULL) ;
+
+commit ;
+
+-- 1 is deleted, so Update
+insert into csaba_src (col1, col2, col3, val, updated_at)
+values ('1','1','1',3,'2026-01-03 23:12:12') ; -- New row, INSERT
+insert into csaba_src (col1, col2, col3, val, updated_at)
+values ('2','1','1',2, '2026-01-02 22:12:12') ; -- Update val
+insert into csaba_src (col1, col2, col3, val, updated_at)
+values ('3','1','1',1, '2026-01-01 12:12:12') ; -- Nothing here
+insert into csaba_src (col1, col2, col3, val, updated_at)
+values ('4','1','1',1, '2026-01-01 12:12:12') ; -- Nothing here
+-- 5 is deleted, so Update
+insert into csaba_src (col1, col2, col3, val, updated_at) -- New row, INSERT
+values ('6','1','1',1, '2026-01-01 23:12:12') ;
+insert into csaba_src (col1, col2, col3, val, updated_at) -- Duplicate. Must be ignored
+values ('7','1','1',1, '2026-01-01 12:12:12') ;
+insert into csaba_src (col1, col2, col3, val, updated_at)
+values ('7','1','1',1, '2026-01-01 12:12:12') ;
+-- 8 is deleted, so Update
+commit ;
+
+select * from csaba_trg order by col1, start_date ;
+"""
+spark.sql ("""
+MERGE INTO csaba_trg AS trg
+USING (
+WITH ua AS (
+    SELECT col1, col2, col3, val, updated_at,
+           1 AS ind1,
+           NULL AS ind2
+    FROM   csaba_src 
+    UNION ALL 
+    SELECT col1, col2, col3, val, updated_at,
+           NULL AS ind1,
+           1 AS ind2
+    FROM   csaba_trg 
+    WHERE  end_date IS NULL
+),
+cnt AS (
+    SELECT col1, col2, col3, val, updated_at,
+           ind1, ind2,
+           COUNT (ua.ind1) OVER (PARTITION BY col1, col2, col3, val ORDER BY updated_at) AS cnt_ind1,
+           COUNT (ua.ind2) OVER (PARTITION BY col1, col2, col3, val ORDER BY updated_at) AS cnt_ind2
+    FROM   ua
+)
+SELECT ua.col1, ua.col2, ua.col3, ua.val, ua.updated_at,
+       CASE WHEN ua.cnt_ind1 = 1 AND ua.cnt_ind2 = 0
+            THEN 'I'
+            WHEN ua.cnt_ind1 = 0 AND ua.cnt_ind2 = 1
+            THEN 'U'
+            ELSE '-'
+       END AS transaction_type,
+       updated_at AS start_date,
+       LEAD (updated_at - interval '1' second) OVER (PARTITION BY col1 ORDER BY updated_at) AS end_date,
+       COUNT (col1) OVER (PARTITION BY col1, col2, col3) AS row_cnt
+FROM   ua
+WHERE  ua.cnt_ind1 != ua.cnt_ind2) ) AS src
+ON     trg.col1 = src.col1 
+   AND trg.col2 = src.col2
+   AND trg.col3 = src.col3
+   AND src.transaction_type = 'U' 
+WHEN MATCHED AND trg.end_date IS NULL 
+       THEN UPDATE SET end_date = CASE WHEN row_cnt = 1 
+	                               THEN DATE_TRUNC ('second', current_timestamp) 
+                                       ELSE src.end_date
+                                  END
+WHEN NOT MATCHED AND transaction_type = 'I' 
+       THEN INSERT (col1, col2, col3, val, updated_at, start_date, end_date) 
+            VALUES (src.col1, src.col2, src.col3, src.val, src.updated_at, src.start_date, src.end_date) 
+;
+
+select * from csaba_trg order by col1, start_date ;
 """
 ).show () # Show overall statistics of the merge
 
 df = spark.read.format("delta").load("D:/Data/DeltaLake/csaba_target")
 print ("Updated target dataframe:")
 df.orderBy(["col1", "start_date"], ascending=[1, 1]).show()
-
