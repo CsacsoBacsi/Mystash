d82d3b0ebd16e237b91d26390f5d9c97349edbfb
Csaba Riedlinger <csaba.riedlinger@gmail.com>
2026-03-08 11:43:09 +0100

    Enhancing merge code

--- Spark all rounder/CheatSheet/Merge SQL incremental.py
+++ Spark all rounder/CheatSheet/Merge SQL incremental.py
@@ -27,26 +27,26 @@ Last 3 timestamp columns are: updated_at, start_date, end_date
             ('1', '1', '1', 4, 2026-01-04 23:12:12, 2026-01-04 23:12:12, 2026-01-05 23:12:11),
             ('1', '1', '1', 3, 2026-01-05 23:12:12, 2026-01-05 23:12:12, None)
 
-4. Single new row for a key. Insert the new row
+3. Single new row for a key. Insert the new row
    Target: -
    Source: ('6', '1', '1', 1, 2026-01-01 23:12:12)
    Result: ('6', '1', '1', 1, 2026-01-01 23:12:12, 2026-01-01 23:12:12, None)
 
-5. Two new rows for the same key. Insert both rows with the earlier one being end dated by the later one
+4. Two new rows for the same key. Insert both rows with the earlier one being end dated by the later one
    Target: -
    Source: ('9', '1', '1', 1, 2026-01-01 12:12:12),
            ('9', '1', '1', 2, 2026-01-02 12:12:12)
    Result: ('9', '1', '1', 1, 2026-01-01 12:12:12, 2026-01-01 12:12:12, 2026-01-02 12:12:11),
            ('9', '1', '1', 2, 2026-01-02 12:12:12, 2026-01-02 12:12:12, None)
 
-3. Source set has no change for a key. No update or insert is needed
+5. Source set has no change for a key. No update or insert is needed
    Target: ('3', '1', '1', 1, 2026-01-01 12:12:12, 2026-01-01 12:12:12, None),
            ('4', '1', '1', 1, 2026-01-01 12:12:12, 2026-01-01 12:12:12, None),
            ('5', '1', '1', 1, 2026-01-01 12:12:12, 2026-01-01 12:12:12, None),
            ('8', '1', '1', 1, 2026-01-01 12:12:12, 2026-01-01 12:12:12, None)
    Result: No change for these keys
 
-4. Source set has duplicate rows for a given key. These rows should be ignored, no update or insert is needed
+6. Source set has duplicate rows for a given key. These rows should be ignored, no update or insert is needed
    Source: ('7', '1', '1', 1, 2026-01-01 12:12:12),
            ('7', '1', '1', 2, 2026-01-01 12:12:12)
    Result: -
@@ -172,13 +172,13 @@ def merge (source_table: str, target_table: str, all_cols: list[str], partition_
     USING (
     WITH ua AS (
         SELECT {select_cols},
-               1 AS ind1,                                               -- In source set
+               1 AS ind1,
                NULL AS ind2
         FROM   {source_table} 
         UNION ALL 
         SELECT {select_cols},
                NULL AS ind1,
-               1 AS ind2                                                -- In target set
+               1 AS ind2
         FROM   {target_table} 
         WHERE  end_date IS NULL
     ),
@@ -229,3 +229,52 @@ merge (source_table, target_table, all_cols, partition_by_cols, primary_key, sor
 df = spark.read.format("delta").load(str(Path(delta_path) / target_delta_table))
 print("Updated target dataframe:")
 df.orderBy(["key1", "start_date"], ascending=[1, 1]).show()
+
+exit (0)
+
+'''
+    MERGE INTO {target_table} AS trg
+    USING (
+    WITH ua AS (
+        SELECT key1, key2, key3, val1, updated_at,
+               1 AS ind1,
+               NULL AS ind2                                                             # Present in source
+        FROM   {source_table} 
+        UNION ALL 
+        SELECT key1, key2, key3, val1, updated_at,
+               NULL AS ind1,
+               1 AS ind2                                                                # Present in target
+        FROM   {target_table} 
+        WHERE  end_date IS NULL                                                         # Current rows only (no end date)
+    ),
+    cnt AS (
+        SELECT key1, key2, key3, val1, updated_at,
+               COUNT (ind1) OVER (PARTITION BY key1, key2, key3, val1) AS cnt_ind1,     # Without updated_at. Mark where row exists (source, target, both)
+               COUNT (ind2) OVER (PARTITION BY key1, key2, key3, val1 AS cnt_ind2
+        FROM   ua
+    )
+    SELECT key1, key2, key3, val1, updated_at,
+           CASE WHEN cnt_ind1 > 0 AND cnt_ind2 = 0                                      # Single or multiple rows exist in source
+                THEN 'I'
+                WHEN cnt_ind1 = 0 AND cnt_ind2 = 1                                      # Row exists in target only (no change)
+                THEN 'U'
+                ELSE '-'                                                                # Same row exists in both source and target or duplicates exist in source
+           END AS transaction_type,
+           updated_at AS start_date,
+           LEAD ({sort_col} - interval '1' second) OVER (PARTITION BY key1, key2, key3 ORDER BY {sort_col}) AS end_date, # Set start/end date order for multiple rows in source
+           COUNT (*) OVER (PARTITION BY key1, key2, key3 AS row_cnt,                    # Count rows under this key. End date row only if there exist at least one source row
+           COUNT (*) OVER (PARTITION BY key1, key2, key3, val1, updated_at AS dupe_cnt  # Insert new rows only if they are not dupes
+    FROM   cnt
+    WHERE  cnt_ind1 != cnt_ind2) AS src                                                 # If counts are equal, there is no change
+    ON     {merge_on}
+           AND src.transaction_type = 'U' 
+    WHEN MATCHED AND trg.end_date IS NULL                                               # End date only if a replacing row exists in source
+           THEN UPDATE SET end_date = CASE WHEN src.row_cnt > 1                         # There is at least one source row besides the target row (row_cnt > 1)
+                                           THEN src.end_date
+                                      END
+    WHEN NOT MATCHED AND transaction_type = 'I' AND src.dupe_cnt = 1                    # Insert only if the source row is not a duplicate
+           THEN INSERT (key1, key2, key3, val1, updated_at, start_date, end_date) 
+                VALUES (src.key1, src.key2, src.key3, src.val1, src.updated_at, src.start_date, src.end_date) 
+    ;
+
+'''
